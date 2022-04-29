f7f22e9ed76ec3c7fd176472e17d899bf42fe222
capecchi <william.capecchi@gmail.com>
2022-04-29 15:41:56 -0400

    in other_analysis/rmag_v_rlcfs added routines to create conbeam maps and plot beam fractions vs rmag and rlcfs

---
+++ conbeam/conbeam_tools.py
@@ -0,0 +1,28 @@
+import subprocess
+import os
+
+cwd = os.getcwd()
+reco_dir = 'Y:/reconstructions/runday/'
+matlab_dir = 'Z:/matlab/conbeam/'
+fig_dir = "Z:/matlab/botdata/"
+
+
+def create_map(eqdsk, shot, runnum):
+	nel = 1.e19  # num/m^3*L
+	ebeam = 17000.  # eV of beam
+
+	os.chdir(matlab_dir)
+	eqdsk = eqdsk.replace('\\', '/')
+	print(f'making map for: {eqdsk}')
+	command_string = ''.join(
+		f'shots={[shot]};Ecsts={[ebeam]};runnums={[runnum]};times={[-1]};ips={[-1]};nels={[nel]};bot_create_map;'.split(
+			'\n'))
+	msgs = subprocess.run(['matlab', '-batch', command_string], capture_output=True)
+	# print('exist status:', msgs.returncode)
+	# print(f'{msgs.stdout.decode()}')
+	# print(f'{msgs.stderr.decode()}')
+	os.chdir(cwd)
+
+	
+if __name__ == '__main__':
+	pass
--- neutral_beam/other_analysis.py
+++ neutral_beam/other_analysis.py
@@ -8,6 +8,8 @@ import os
 import plotly
 import json
 from helpful_stuff import read_eqdsk2
+from conbeam import conbeam_tools
+from scipy.io import loadmat
 
 
 def rmag_v_rlcfs(rescan=True, ploteach=False):
@@ -20,10 +22,14 @@ def rmag_v_rlcfs(rescan=True, ploteach=False):
 	if rescan:
 		reco_dir = 'Y:/reconstructions/runday/'
 		
-		# eqdsks = glob.glob(f'{reco_dir}**/LTX*.eqdsk', recursive=True)
-		eqdsks = glob.glob(f'{reco_dir}SHOTS/105894/**/LTX*.eqdsk', recursive=True)
+		print(f'gathering eqdsks...')
+		eqdsks = glob.glob(f'{reco_dir}**/LTX*.eqdsk', recursive=True)
+		print(f'...complete')
+		# eqdsks = glob.glob(f'{reco_dir}SHOTS/105894/**/LTX*.eqdsk', recursive=True)  # fast scan for debugging
 		rmag, rlcfs = [], []
-		for eq in eqdsks:
+		for i, eq in enumerate(eqdsks):
+			eq = eq.replace('\\', '/')
+			print(f'extracting info from {eq}, #{i}/{len(eqdsks)}')
 			out = read_eqdsk2(eq)
 			if ploteach:
 				r_1d = np.linspace(out['rleft'], out['rleft'] + out['rdim'], num=out['nr'])
@@ -35,16 +41,88 @@ def rmag_v_rlcfs(rescan=True, ploteach=False):
 			rmag.append(out['raxis'])
 			rlcfs.append(np.max(out['rzout'][0, :]))
 		print(f'--saved data--')
-		np.savez(fsav, rmag=rmag, rlcfs=rlcfs)
+		np.savez(fsav, rmag=rmag, rlcfs=rlcfs, eqdsks=eqdsks)
+		rmag, rlcfs, eqdsks = np.array(rmag), np.array(rlcfs), np.array(eqdsks)
 	else:
 		dat = np.load(fsav)
-		rmag = list(dat['rmag'])
-		rlcfs = list(dat['rlcfs'])
-	plt.plot(rlcfs, rmag, 'o')
-	plt.xlabel('$r_{lcfs}$')
-	plt.ylabel('$r_{mag}$')
-	plt.show()
+		rmag = np.array(dat['rmag'])
+		rlcfs = np.array(dat['rlcfs'])
+		eqdsks = np.array(dat['eqdsks'])
 	
+	tol = .001  # 1 cm
+	rmagchoice = .4
+	rlcfschoice = .57
+	irmag = np.where(abs(rmag - rmagchoice) < tol)[0]
+	irlcfs = np.where(abs(rlcfs - rlcfschoice) < tol)[0]
+	print(f'found {len(irmag)} pts within {tol} of rmag')
+	print(f'found {len(irlcfs)} pts within {tol} of rlcfs')
+	
+	fig, ax = plt.subplots()
+	ax.plot(rlcfs, rmag, 'o', zorder=1)
+	ax.fill_between(ax.get_xlim(), [rmagchoice - tol, rmagchoice - tol], [rmagchoice + tol, rmagchoice + tol],
+	                facecolor='k', alpha=.25, zorder=2)
+	yl = ax.get_ylim()
+	ax.fill_between([rlcfschoice - tol, rlcfschoice + tol], [yl[0], yl[0]], [yl[1], yl[1]], facecolor='k', alpha=.25,
+	                zorder=2)
+	ax.set_ylim(yl)
+	ax.set_xlabel('$r_{lcfs}$')
+	ax.set_ylabel('$r_{mag}$')
+	
+	fig2, (ax1, ax2) = plt.subplots(ncols=2, sharey=True)
+	eqs = eqdsks[irmag]
+	rlcfs_pts = rlcfs[irmag]
+	runnums = [int(eq.split('_')[-1].split('.')[0]) for eq in eqs]
+	shots = [int(eq.split('_')[-2]) for eq in eqs]
+	shine, prompt, coup = [], [], []
+	for sh, rn, eq in zip(shots, runnums, eqs):
+		matfile = f'Z:/matlab/botdata/{sh}_{rn:02}.mat'
+		if not os.path.exists(matfile):
+			conbeam_tools.create_map(eq, sh, rn)
+		try:
+			matdat = loadmat(matfile)
+			shine.append(matdat['shinethru'][0][0])
+			prompt.append(matdat['prompt_loss'][0][0])
+			coup.append(matdat['coupled'][0][0])
+		except FileNotFoundError:
+			shine.append(np.nan)
+			prompt.append(np.nan)
+			coup.append(np.nan)
+	ax1.plot(rlcfs_pts, shine, label='shinethrough')
+	ax1.plot(rlcfs_pts, prompt, label='prompt loss')
+	ax1.plot(rlcfs_pts, coup, label='coupled')
+	ax1.set_title(f'rmag = {rmagchoice}+/-{tol}')
+
+	eqs = eqdsks[irlcfs]
+	rmag_pts = rmag[irlcfs]
+	runnums = [int(eq.split('_')[-1].split('.')[0]) for eq in eqs]
+	shots = [int(eq.split('_')[-2]) for eq in eqs]
+	shine, prompt, coup = [], [], []
+	for sh, rn, eq in zip(shots, runnums, eqs):
+		matfile = f'Z:/matlab/botdata/{sh}_{rn:02}.mat'
+		if not os.path.exists(matfile):
+			conbeam_tools.create_map(eq, sh, rn)
+		try:
+			matdat = loadmat(matfile)
+			shine.append(matdat['shinethru'][0][0])
+			prompt.append(matdat['prompt_loss'][0][0])
+			coup.append(matdat['coupled'][0][0])
+		except FileNotFoundError:
+			shine.append(np.nan)
+			prompt.append(np.nan)
+			coup.append(np.nan)
+	ax2.plot(rmag_pts, shine, label='shinethrough')
+	ax2.plot(rmag_pts, prompt, label='prompt loss')
+	ax2.plot(rmag_pts, coup, label='coupled')
+	ax2.set_title(f'rlcfs = {rlcfschoice}+/-{tol}')
+
+	ax1.legend()
+	ax1.set_xlabel('$R_{lcfs}$ (m)')
+	ax1.set_ylabel('beam fraction')
+	ax2.set_xlabel('$R_{mag}$ (m)')
+	plt.tight_layout()
+	
+	plt.show()
+
 
 def density_analysis_8feb22():
 	shots_8feb = np.arange(25) + 105177  # shots 105177-105201
@@ -82,7 +160,7 @@ def density_analysis_8feb22():
 	plt.show()
 
 
-if __name__ is '__main__':
+if __name__ == '__main__':
 	# density_analysis_8feb22()
 	print('running main')
 	rmag_v_rlcfs(rescan=False)
