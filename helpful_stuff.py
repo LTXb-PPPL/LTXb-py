60eb4c493e1d88e9e3bb92d8a4a94c8fce7a0386
willi <william.capecchi@gmail.com>
2025-08-22 14:51:33 -0500

    added wire cal stuff

--- helpful_stuff.py
+++
@@ -1,9 +0,0 @@
-import matplotlib
-import matplotlib.pyplot as plt
-import numpy as np
-
-matplotlib.use('TkAgg')  # allows plotting in debug mode
-clrs = plt.rcParams['axes.prop_cycle'].by_key()['color']
-
-if __name__ == '__main__':
-	pass
