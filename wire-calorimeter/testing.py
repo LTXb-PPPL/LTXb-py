6f149276b1c9131885cfba878f4c0345fa57767f
willi <william.capecchi@gmail.com>
2025-08-22 14:53:24 -0500

    added wire cal stuff

---
+++ wire-calorimeter/testing.py
@@ -0,0 +1,9 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib
+
+matplotlib.use('TkAgg')  # allows plotting in debug mode
+clrs = plt.rcParams['axes.prop_cycle'].by_key()['color']
+
+if __name__ == '__main__':
+    pass
